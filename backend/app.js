13b4ddc88d26d01c8ae675ac8f0cfe2f32d83d4c
RISHABH SINGH <2k23.cs2313878@gmail.com>
2025-11-29 04:07:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const http = require('http');
-const cors =require('cors');
+const cors = require('cors');
 const socketio = require('socket.io');
 
 const app = express();
 
-const server = http.createServer(app);
-const io = socketio(server,{
-    cors :{
-        origin:"*"
-    }
-})
+app.use(cors());
 
-io.on("connection",(socket)=>{
+// Root route to check server
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
     console.log('New client connected');
 
-    socket.on("chat",(data) =>{
-        io.emit("chat",data)
-    })
+    socket.on("chat", (data) => {
+        io.emit("chat", data);
+    });
 
-    socket.on("disconnect",()=>{
+    socket.on("disconnect", () => {
         console.log('Client disconnected');
-    })
-})
+    });
+});
 
-server.listen(8000,()=>{
+server.listen(8000, () => {
     console.log('Server listening on port 8000');
-})
+});
